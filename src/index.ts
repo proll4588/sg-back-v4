2acff8987761ad7c0830e278e694a68f9ae184c1
Kostya <prol4588@gmail.com>
2024-10-20 00:19:57 +0700

    feat: :sparkles: Добавлен cors
    Добавлен cors пакеи и адаптирован текущий сервер к cors

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ import { ApolloContext } from './context/type.js';
 import { context } from './context/context.js';
 import typeDefs from './type-defs/typeDefs.js';
 import { resolvers } from './resolvers/resolvers.js';
+import express from 'express';
+import http from 'http';
+import { expressMiddleware } from '@apollo/server/express4';
+import cors from 'cors';
 
 const server = new ApolloServer<ApolloContext>({
   typeDefs,
@@ -17,9 +21,30 @@ const server = new ApolloServer<ApolloContext>({
   introspection: true,
 });
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-  context: context,
-});
+const app = express();
+const httpServer = http.createServer(app);
 
-console.log(`🚀  Server ready at: ${url}`);
+await server.start();
+
+app.use(
+  '/graphql',
+  // highlight-start
+  cors<cors.CorsRequest>({
+    origin: '*',
+  }),
+  // highlight-end
+  express.json(),
+  expressMiddleware(server, { context })
+);
+
+await new Promise<void>((resolve) =>
+  httpServer.listen({ port: 4000 }, resolve)
+);
+console.log(`🚀 Server ready at http://localhost:4000/graphql`);
+
+// const { url } = await startStandaloneServer(server, {
+//   listen: { port: 4000 },
+//   context: context,
+// });
+
+// console.log(`🚀  Server ready at: ${url}`);
